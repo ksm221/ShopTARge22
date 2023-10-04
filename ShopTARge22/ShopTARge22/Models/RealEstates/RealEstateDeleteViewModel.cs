3fd8823b8564adb8a4c8a3cb51351d4e741b54ad
Karl <karl.mellikov@gmail.com>
2023-10-05 01:35:55 +0300

    delete one by one (realestate images)

---
+++ ShopTARge22/ShopTARge22/Models/RealEstates/RealEstateDeleteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ShopTARge22.Models.Spaceships;
+
+namespace ShopTARge22.Models.RealEstates
+{
+    public class RealEstateDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Address { get; set; }
+        public float SizeSqrM { get; set; }
+        public int RoomCount { get; set; }
+        public int Floor { get; set; }
+        public string BuildingType { get; set; }
+        public DateTime BuiltInYear { get; set; }
+
+        public List<RealEstateImageViewModel> Image { get; set; }
+            = new List<RealEstateImageViewModel>();
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
