af7268f983ffafd77a0507b171e90cac9cf2e113
Opilane TTHK <opilane@tthk.ee>
2023-11-02 12:56:48 +0200

    Kindergarten added

---
+++ ShopTARge22Kindergarten/ShopTARge22/ShopTARge22.ApplicationServices/Services/KindergartensServices.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARge22.Core.Domain;
+using ShopTARge22.Core.Dto;
+using ShopTARge22.Core.ServiceInterface;
+using ShopTARge22.Data;
+
+
+namespace ShopTARge22.ApplicationServices.Services
+{
+    public class KindergartensServices : IKindergartensServices
+    {
+        private readonly ShopTARge22Context _context;
+        private readonly IFileServices _fileServices;
+
+        public KindergartensServices
+            (
+                ShopTARge22Context context,
+                IFileServices fileServices
+            )
+        {
+            _context = context;
+            _fileServices = fileServices;
+        }
+
+
+        public async Task<Kindergarten> Create(KindergartenDto dto)
+        {
+            Kindergarten kindergarten = new Kindergarten();
+
+            kindergarten.Id = Guid.NewGuid();
+            kindergarten.GroupName = dto.GroupName;
+            kindergarten.ChildrenCount = dto.ChildrenCount;
+            kindergarten.KindergartenName = dto.KindergartenName;
+            kindergarten.Teacher = dto.Teacher;
+            kindergarten.CreatedAt = DateTime.Now;
+            kindergarten.UpdatedAt = DateTime.Now;
+
+
+            await _context.Kindergartens.AddAsync(kindergarten);
+            await _context.SaveChangesAsync();
+
+            return kindergarten;
+        }
+
+        public async Task<Kindergarten> DetailsAsync(Guid id)
+        {
+            var result = await _context.Kindergartens
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+        public async Task<Kindergarten> Update(KindergartenDto dto)
+        {
+            Kindergarten domain = new();
+
+            domain.Id = dto.Id;
+            domain.GroupName = dto.GroupName;
+            domain.ChildrenCount = dto.ChildrenCount;
+            domain.KindergartenName = dto.KindergartenName;
+            domain.Teacher = dto.Teacher;
+            domain.CreatedAt = dto.CreatedAt;
+            domain.UpdatedAt = DateTime.Now;
+
+            _context.Kindergartens.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+
+        public async Task<Kindergarten> Delete(Guid id)
+        {
+            var KindergartenId = await _context.Kindergartens
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            var images = await _context.FileToApis
+                .Where(x => x.SpaceshipId == id)
+                .Select(y => new FileToApiDto
+                {
+                    Id = y.Id,
+                    SpaceshipId = y.SpaceshipId,
+                    ExistingFilePath = y.ExistingFilePath
+                }).ToArrayAsync();
+
+            await _fileServices.RemoveImagesFromApi(images);
+            _context.Kindergartens.Remove(KindergartenId);
+            await _context.SaveChangesAsync();
+
+            return KindergartenId;
+        }
+    }
+}
